7a62de416b567989a9e020e27de9ff9b5f0454c4
Rohit <raheet@hotmail.co.uk>
2022-10-28 04:56:34 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import "./App.scss";
 
-function App() {
+const menu = ["home", "education", "portfolio", "contact"]
+
+const App = () => {
+  const [page,loadPage] = useState();
+  const [menuState, toggleMenu] = useState(false);
+
+  const navigate = useNavigate();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div id="hamburger-menu">
+          <input id="hamburger--toggle" className="hamburger" type="checkbox"
+            checked={menuState}
+            readOnly
+            onClick={() => {
+              menuState? toggleMenu(false): toggleMenu(true);
+            }}
+          />
+          <label htmlFor="hamburger--toggle">
+            <div>
+              <div className="hamburger layers" />
+              <div className="hamburger layers" />
+              <div className="hamburger layers" />
+            </div>
+          </label>
+        </div>
+        <div id="logo">Rohit Naidu</div>
       </header>
+      <div id="container">
+      <nav id="menu" className={menuState? "":"slideOut"}>
+        <ul>
+          { menu.map((item) => (
+            <label htmlFor = { item }>
+              <li key = { item } >
+                <input
+                  className="menu"
+                  name = "menu"
+                  type = "radio"
+                  value = { item }
+                  id = { item }
+                  checked = { page === item }
+                  onChange = {() => {
+                    loadPage(item)
+                    (item === "introduction") ? navigate(""):navigate("/" + item)
+                  }}
+                /> { item.at(0).toUpperCase()+item.slice(1,item.length) }
+              </li>
+            </label>
+            ))}
+        </ul>
+      </nav>
+        <Routes>
+          <Route path ="*" element = { <Landing /> }
+            key = { document.location.href }
+          />
+        </Routes>
+        {page}
+      </div>
     </div>
   );
 }
+
+const Landing = () => {
+  return (
+    <article>
+      <h3>Error 404: Page not Found</h3>
+      <p> Please use the navigation bar to locate to another page.</p>
+    </article>
+  )
+}
+
 
 export default App;
